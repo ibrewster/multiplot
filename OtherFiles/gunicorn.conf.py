d4fd1dbbdf2484b29c130953f0e04ef45d02776b
Israel Brewster <ijbrewster@alaska.edu>
2023-03-09 17:53:53 +0000

    Changes to make work in production

---
+++ OtherFiles/gunicorn.conf.py
@@ -0,0 +1,17 @@
+import os
+
+script_dir = os.path.dirname(__file__)
+script_dir = os.path.abspath(os.path.join(script_dir,'..'))
+
+wsgi_app = "PREVENTSWeb:app"
+chdir = script_dir
+# user = "mapgen"
+# group = "nginx"
+bind = ['unix:/var/run/preevents/preevents.sock']
+workers = 8
+threads = 50
+worker_connections = 51
+timeout = 300
+accesslog = "/var/log/preevents/access.log"
+errorlog = "/var/log/preevents/error.log"
+capture_output = True
