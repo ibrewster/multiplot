0861382f13cfc34541bdc05062f4bddc0f453591
Israel Brewster <ijbrewster@alaska.edu>
2023-06-20 19:49:36 +0000

    Cleanup/fix config files to work with new multiplot naming

--- OtherFiles/gunicorn.conf.py
+++ OtherFiles/gunicorn.conf.py
@@ -3,15 +3,15 @@ import os
 script_dir = os.path.dirname(__file__)
 script_dir = os.path.abspath(os.path.join(script_dir,'..'))
 
-wsgi_app = "PREEVENTSWeb:app"
+wsgi_app = "MultiplotWeb:app"
 chdir = script_dir
 # user = "mapgen"
 # group = "nginx"
-bind = ['unix:/var/run/preevents/preevents.sock']
+bind = ['unix:/var/run/multiplot/multiplot.sock']
 workers = 8
 threads = 50
 worker_connections = 51
 timeout = 300
-accesslog = "/var/log/preevents/access.log"
-errorlog = "/var/log/preevents/error.log"
+accesslog = "/var/log/multiplot/access.log"
+errorlog = "/var/log/multiplot/error.log"
 capture_output = True
